a6dae8e6b19fead5b04faca52e4e03a97c29c440
Marcelearn <jaworskimarcel@gmail.com>
2022-11-09 19:47:23 +0100

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useReducer, useState } from 'react';
+
+import './App.css';
+import Todo from './Todo.jsx'
+const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo'
+}
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if(todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+  }
+
+}
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false}
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name }})
+    setName('')
+  }
+  
+  console.log(todos)
+  return (
+    <div className="App">
+     <h1>todo</h1>
+     <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
+     
+     </>
+
+       
+    </div>
+  )
+}
+
+export default App
