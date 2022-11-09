72b938343f4f7ab5ac68cbdad8e68f13d7b67494
Marcelearn <jaworskimarcel@gmail.com>
2022-11-09 20:30:26 +0100

    new task h2 and placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export const ACTIONS = {
 function reducer(todos, action) {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.name)]
+      return [newTodo(action.payload.name), ...todos]
     case ACTIONS.TOGGLE_TODO:
       return todos.map(todo => {
         if(todo.id === action.payload.id) {
@@ -42,18 +42,15 @@ function App() {
   console.log(todos)
   return (
     <div className="App">
-     <h1>todo</h1>
+     <h1>to do</h1>
      <>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input  placeholder="new Task" type="text" value={name} onChange={e => setName(e.target.value)} />
       </form>
       {todos.map(todo => {
         return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
       })}
-     
-     </>
-
-       
+     </>       
     </div>
   )
 }
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,12 +4,11 @@ import { ACTIONS } from './App.jsx'
 export default function Todo({ todo, dispatch }) {
   return (
     <div>
-        <span style={{ color: todo.complete ? '#000' : '#C2C2C2'}}>
+        <h2 style={{ color: todo.complete ? '#000' : '#C2C2C2'}}>
             {todo.name}
-        </span>
+        </h2>
         <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id }})}>Done</button>
         <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id }})}>Delete</button>
-
     </div>
   )
 }
